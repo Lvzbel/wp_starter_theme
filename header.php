70a89a04bfdc6b901f14aa27af4b9a0cc91e7332
Lvzbel <rcoto@protonmail.com>
2020-05-05 21:24:26 -0700

    Feat: added header and footer basic wp functions

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
   <title><?php echo wp_get_document_title() ?></title>
 </head>
 
-<body>
+<body <?php body_class();?>>
