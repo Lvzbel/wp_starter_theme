59738929da95fde031234e37be498a32b32fac99
Lvzbel <rcoto@protonmail.com>
2020-05-05 22:41:47 -0700

    Feat: added wp loop to all the pages

--- single.php
+++ single.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 
-single.php
+<?php while(have_posts()): the_post(); ?>
+
+<h1><?php the_title(); ?></h1>
+
+<div><?php the_content(); ?></div>
+
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
