ac35051ad17766ebf8510c83519ff085ba03883d
Lvzbel <rcoto@protonmail.com>
2020-05-05 21:21:09 -0700

    Feat: added document title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title><?php echo wp_get_document_title() ?></title>
 </head>
 
 <body>
