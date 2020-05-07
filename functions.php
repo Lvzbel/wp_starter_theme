a5a411f1c31c93d61cbd7b8b06aacfd5c85946f2
Lvzbel <rcoto@protonmail.com>
2020-05-06 18:39:38 -0700

    Refact: removed /style and added comments

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php 
 
 // Enqueue Styles and Scripts
-
 function add_theme_scripts() {
-  wp_enqueue_style( '/style', get_stylesheet_uri() );
- 
+  // Custom Styles CSS
   wp_enqueue_style( 'styles', get_template_directory_uri() . '/dist/app.css', array(), '1.0.0', 'all');
- 
+  // Cusom JavaScript
   wp_enqueue_script( 'script', get_template_directory_uri() . '/dist/app.js', array (), '1.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+// Enable extra features
