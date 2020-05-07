0f735fb8db93bf792ffd2316b1b74a29f4da4cbf
Lvzbel <rcoto@protonmail.com>
2020-05-06 19:10:15 -0700

    Feat: new vendor scss file was created

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ let mix = require("laravel-mix");
 
 mix
   .js("src/app.js", "dist")
+  .sass("src/vendor.scss", "dist")
   .sass("src/app.scss", "dist")
   .setPublicPath("dist")
   .browserSync({
