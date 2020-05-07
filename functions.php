50773bcc76a83ae4748b7c2b554dbf97f1de98ac
Lvzbel <rcoto@protonmail.com>
2020-05-06 19:11:37 -0700

    Feat: vendor.scss has been added

--- functions.php
+++ functions.php
@@ -2,8 +2,10 @@
 
 // Enqueue Styles and Scripts
 function add_theme_scripts() {
+  // Vendor CSS
+  wp_enqueue_style( 'vendor_styles', get_template_directory_uri() . '/dist/vendor.css', array(), '1.0.0', 'all');
   // Custom Styles CSS
-  wp_enqueue_style( 'styles', get_template_directory_uri() . '/dist/app.css', array(), '1.0.0', 'all');
+  wp_enqueue_style( 'styles', get_template_directory_uri() . '/dist/app.css', array('vendor_styles'), '1.0.0', 'all');
   // Cusom JavaScript
   wp_enqueue_script( 'script', get_template_directory_uri() . '/dist/app.js', array (), '1.0.0', true);
 }
